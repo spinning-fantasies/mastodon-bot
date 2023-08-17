afdd3c655e545334cd8ea98b06ca58f4e1058b81
Mathieu Mescam <mate@e.email>
2023-08-17 18:33:54 +0200

    lets go 2

---
+++ mastodon_bot/main.py
@@ -0,0 +1,26 @@
+import os
+
+from dotenv import load_dotenv
+from mastodon import Mastodon
+
+load_dotenv()
+
+
+def register_app() -> None:
+    Mastodon.create_app(
+        "mastodon_bot",
+        api_base_url="https://3615.computer/",
+        to_file="mastodon_bot_clientcred.secret",
+    )
+
+
+def main() -> None:
+    password = os.getenv("MASTODON_PASSWORD")
+    if not password:
+        raise ValueError("Password is required")
+
+    register_app()
+
+
+if __name__ == "__main__":
+    main()
